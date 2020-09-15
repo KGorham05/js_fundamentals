08e8cc1b5dc52a7e5584c6e950ec97d786d67344
Gorham <kevin.gorham@gmail.com>
2020-09-14 21:02:40 -0700

    started JS practice

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,29 @@
-// console.log('Hello World!')
-alert('Hello world!')
+console.log('Hello World!');
+
+// declare variables
+var name = "Foos"; 
+var tutor = "Kevin";
+// boolean
+var isStudent = true;
+// string
+var greeting = `Hello ${name} nice to meet you`; // "Hello " + name + " nice to meet you"
+// number
+var age = 21;
+// array
+var kids = ["Leo", "Adnan"];
+// object
+var webDevClass = {
+  teacher: "Nate",
+  students: ["Foos", "Sarah", "Steven"],
+  sayHello: function() {
+    this.students.map(student => {
+      console.log(`Welcome to class ${student}`)
+    })
+  }
+}
+
+console.log(webDevClass.students)
+console.log(webDevClass.teacher)
+webDevClass.sayHello();
+// declare function
+// execute function
